4c88fab0749ee7c33f45bc96cede8ad101b79d67
Matthias Druwé <matthias.druwe@gmail.com>
2022-04-28 14:36:17 +0200

    Add project files.

---
+++ BMICalculator/MyCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BMICalculator
+{
+    internal class MyCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action action;
+
+
+        public MyCommand(Action action)
+        {
+            this.action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if(action != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();  
+        }
+    }
+}
