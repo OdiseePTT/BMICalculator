66d10411367c502220ebd8b838d359c9634eafb1
Matthias Druwé <matthias.druwe@gmail.com>
2022-04-28 20:01:10 +0200

    Updates

--- BMICalculator/MainViewModel.cs
+++ BMICalculator/MainViewModel.cs
@@ -1,45 +1,62 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace BMICalculator
 {
-    internal class MainViewModel: INotifyPropertyChanged
+    internal class MainViewModel : INotifyPropertyChanged
     {
         private double? length;
+        private double? weight;
+        private double? bmi;
+        private Brush color;
 
         public double? Length
         {
             get { return length; }
-            set { length = value;
+            set
+            {
+                length = value;
                 OnPropertyChanged();
             }
         }
-
-        private double? weight;
 
         public double? Weight
         {
             get { return weight; }
-            set { weight = value;
+            set
+            {
+                weight = value;
                 OnPropertyChanged();
             }
         }
 
-        private double bmi;
-
-        public double Bmi
+        public double? Bmi
         {
             get { return bmi; }
-            private set { 
+            private set
+            {
                 bmi = value;
+                OnPropertyChanged();
+                UpdateColor();
+            }
+        }
+
+        public Brush Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
                 OnPropertyChanged();
             }
         }
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if(PropertyChanged != null)
+            if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
@@ -52,22 +69,57 @@ namespace BMICalculator
 
         public MainViewModel()
         {
-            ClickCommand = new MyCommand(clickCommandAction); 
-            ResetCommand = new MyCommand(resetCommandAction);  
+            ClickCommand = new MyCommand(ClickCommandAction);
+            ResetCommand = new MyCommand(ResetCommandAction);
         }
 
-        public void clickCommandAction()
+        private void ClickCommandAction()
         {
-            if(Length != null && weight != null) { 
-                Bmi = (double)(Weight / (Length * Length));
+            if (Length != null && weight != null)
+            {
+                Bmi = Math.Round((double)(Weight / (Length * Length)), 2);
             }
         }
 
-        public void resetCommandAction()
+        private void ResetCommandAction()
         {
             Length = null;
             Weight = null;
             Bmi = 0;
         }
+
+        private void UpdateColor()
+        {
+
+            if (Bmi == 0)
+            {
+                Color = null;
+            }
+            else if (Bmi < 18.5)
+            {
+                Color = Brushes.LightBlue;
+            }
+            else if (Bmi < 25)
+            {
+                Color = Brushes.Green;
+            }
+            else if (Bmi < 30)
+            {
+                Color = Brushes.Yellow;
+            }
+            else if (Bmi < 35)
+            {
+                Color = Brushes.Orange;
+            }
+            else if (Bmi < 40)
+            {
+                Color = Brushes.OrangeRed;
+            }
+            else
+            {
+                Color = Brushes.DarkRed;
+            }
+        }
+
     }
 }
